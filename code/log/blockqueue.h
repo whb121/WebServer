9b296683bf1c2bad28db2d5c50b91d99c5f13d82
whb121 <43930070+whb121@users.noreply.github.com>
2023-09-10 10:34:48 +0800

    Update blockqueue.h

--- code/log/blockqueue.h
+++ code/log/blockqueue.h
@@ -15,46 +15,27 @@ template<class T>
 class BlockDeque {
 public:
     explicit BlockDeque(size_t MaxCapacity = 1000);
-
     ~BlockDeque();
-
     void clear();
-
     bool empty();
-
     bool full();
-
     void Close();
-
     size_t size();
-
     size_t capacity();
-
     T front();
-
     T back();
-
     void push_back(const T &item);
-
     void push_front(const T &item);
-
     bool pop(T &item);
-
     bool pop(T &item, int timeout);
-
     void flush();
 
 private:
     std::deque<T> deq_;
-
     size_t capacity_;
-
     std::mutex mtx_;
-
     bool isClose_;
-
     std::condition_variable condConsumer_;
-
     std::condition_variable condProducer_;
 };
 
